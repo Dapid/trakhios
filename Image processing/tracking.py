48dd2f339e75052bec353bae7f43d1dd24bb93d9
davidmenhur <davidmenhur@localhost>
2010-07-18 16:17:06 +0200

    Some starting

---
+++ Image processing/tracking.py
@@ -0,0 +1,22 @@
+#importing
+import numpy as np
+import psyco
+import matplotlib
+#use Agg
+
+def importer():
+    image=open('data/'+namefile())
+    return image
+
+def namefile():
+    
+def find_center(x0, image):
+    
+
+
+print "Tracking"
+
+#Setting up
+tol=0.7
+
+psyco.full()
