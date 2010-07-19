6d8038fa6aec65854ee0ec3717bd0786e8215471
davidmenhur <davidmenhur@localhost>
2010-07-19 21:58:54 +0200

    Setting up the program

--- Image processing/tracking.py
+++ Image processing/tracking.py
@@ -4,19 +4,66 @@ import psyco
 import matplotlib
 #use Agg
 
-def importer():
-    image=open('data/'+namefile())
+print "Tracking. Developing version."
+
+def importer(name, it):
+    image=open('data/'+namefile(name, it))
     return image
 
-def namefile():
+def namefile(name, it):
     
 def find_center(x0, image):
     
+def export(data, txtfile):
+
+def asking_file(txtfile):
+    try:
+        open(dbfile, 'r')
+        print 'The exporting file already exists. Overwrite? (y/n)',
+        while True:
+            over=raw_input()
+            if over=='y' or over=='yes':
+                overwrite=True
+                break
+            elif over=='n' or over=='no':
+                overwrite=False
+                break
+            else: print 'Command unknown. Please, type y/n.',
+
+    except IOError: overwrite=True
+
+    if overwrite==True:
+        dbfile=open(dbfile, 'w')
+        return dbfile
+    else:
+        raw_input('An error will be raised to end the program. Press enter.')
+        raise 'FileHandlingError.'
 
 
-print "Tracking"
 
-#Setting up
+# Setting up
 tol=0.7
+centers=[[20,30],[25, 49]]
+dbfile='data.txt'
 
+dbfile=asking_file(dbfile)
+
+    
 psyco.full()
+
+# Parameters
+
+namecode='test'
+bottom=0
+top=50
+
+
+# Iterating
+
+print 'Starting'
+for it in xrange(bottom, top):
+    frame=importer(namecode, it)
+    for k in len(centers):
+        centers[k]=find_center(centers[k], frame)
+        export(centers[k], dbfile)
+        
