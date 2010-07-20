1fc2f7bc59fe732173646b29effd49983945f3ec
davidmenhur <davidmenhur@localhost>
2010-07-20 20:13:03 +0200

    Completing tracking.py

--- Image processing/tracking.py
+++ Image processing/tracking.py
@@ -16,31 +16,31 @@ import hrun
 print "Tracking. Developing version."
 
 def importer(name, it):
-    image=open('data/'+namefile(name, it))
+    image=mpimg.imread('data/'+namefile(name, it))
     return image
 
 def namefile(name, it):
-    
-
+    return name+'_'+str(it).zfill(6)+'.png'
 
 def export(data, txtfile):
-
-    filename.flush()
-    os.fsync(filename.fileno())
+    line=str(data)+'\n'
+    txtfile.write(line)
+    txtfile.flush()
+    os.fsync(txtfile.fileno())
 
 # Setting up
 tol=0.7
-centers=[array([20,30]),array([25, 49])]
-dbfile='data.txt'
+centers=[array([230,230]),array([374, 749])]
+dbfilename='data.txt'
 
-dbfile=silenus.asking_file(dbfile)
-
+#dbfile=silenus.asking_file('data.txt')
+dbfile=open(dbfilename, 'w')
     
 psyco.full()
 
 # Parameters
 
-namecode='test'
+namecode='firstest'
 bottom=0
 top=50
 
@@ -50,7 +50,10 @@ top=50
 print 'Starting'
 for it in xrange(bottom, top):
     frame=importer(namecode, it)
-    for k in len(centers):
-        centers[k]=hrun.find_center(centers[k], frame)
+    for k in xrange(len(centers)):
+        centers[k]=hrun.find_center(centers[k], frame, tol)
         export(centers[k], dbfile)
-        
+    export('\n', dbfile)
+    print it
+
+print 'Finished'
