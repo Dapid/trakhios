60d1acf9233ab6ca9eda6379bb63130f7210a68d
davidmenhur <davidmenhur@localhost>
2010-07-21 18:02:51 +0200

    Now, Hrun does iterative searching of the center.

--- Image processing/hrun.py
+++ Image processing/hrun.py
@@ -1,13 +1,27 @@
 from __future__ import division
-from numpy import array
+from numpy import array, linalg
+
 import silenus
 
 'Hrun, an intelectual barbarian, is the intelligence core of Trakhios.'
 'He will analize each frame looking for trackable points.'
 
-ver='Hrun v.1.0.1'
+ver='Hrun v.2 b'
+maxit=5
+step_tol=1
+
 
 def find_center(x0, image, tol):
+    x1=find_center_step(x0, image, tol)
+    it=0
+    for p in xrange(maxit):
+        x2=find_center_step(x1, image, tol)
+        if linalg.norm(x1-x2)<step_tol: break
+        else: x1=x2
+    return x2
+
+    
+def find_center_step(x0, image, tol):
     x=x0[0]
     y=x0[1]
     
