1e38aececbbed49f5910ea7fb67efe6bc6791138
davidmenhur <davidmenhur@localhost>
2010-10-30 20:57:13 +0200

    Turned into a function.

--- Image processing/importer_data.py
+++ Image processing/importer_data.py
@@ -2,43 +2,41 @@ from __future__ import division
 import numpy as np
 from numpy import array, reshape
 
-
-print "Starting importing data"
-
 def float_list(lis):
     newlis=[]
     for i in lis:
         newlis.append(float(i))
     return newlis
 
-namefile='data.txt'
-
-file=open(namefile, 'r')
-ncenters=2
-centers=[]
-count=0
-k=0
-
-for i in xrange(ncenters): 
-    centers.append(array([]))
-it=0
-
-while True:
-    print it,
-    it+=1
-    line=file.readline()
-    if line=='\n': pass     # Empty separator.
-    elif line=='': break    # EOF
-    else:
-        line=line[0:-2]     # Removing \n
-        line=line.split('&')
-        line=float_list(line)
-        centers[k]=np.append(centers[k],line, axis=0) # ...
-        k+=1                # Attached as a 1D array.
-        if k==ncenters: k=0
-        
-print
-for k in range(ncenters):
-    centers[k]=centers[k].reshape(len(centers[k])/2,2)
+def importer_data(namefile='data.txt'):
+    print "Starting importing data"
+    
+    file=open(namefile, 'r')
+    ncenters=2
+    centers=[]
+    count=0
+    k=0
+    
+    for i in xrange(ncenters): 
+        centers.append(array([]))
+    it=0
+    
+    while True:
+        print it,
+        it+=1
+        line=file.readline()
+        if line=='\n': pass     # Empty separator.
+        elif line=='': break    # EOF
+        else:
+            line=line[0:-2]     # Removing \n
+            line=line.split('&')
+            line=float_list(line)
+            centers[k]=np.append(centers[k],line, axis=0) # ...
+            k+=1                # Attached as a 1D array.
+            if k==ncenters: k=0
+            
+    print
+    for k in range(ncenters):
+        centers[k]=centers[k].reshape(len(centers[k])/2,2)
 
 print "End"
