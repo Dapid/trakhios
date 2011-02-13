453f53a4f75c47f4db240f517885a4ffb45a6d70
davidmenhur <davidmenhur@localhost>
2011-02-13 18:38:51 +0100

    Matrix channel mix now deppending on mode.

--- Image processing/config.py
+++ Image processing/config.py
@@ -13,12 +13,13 @@ top=900
 if mode==1:
     tol=0.7
     centers=[array([150,380]),array([520, 374])]
+    matrix=(0.334,0.334,0.334, 0)
 
 if mode==2:
     tol=0.13
     centers=[array([327,151]),array([437, 176]),
          array([555,180]),array([686, 168]),array([788,137])]
-
+    matrix=(0.334,0.334,0.334, 0)
 
 # set a number of parameters
 su='Setting up'
@@ -36,7 +37,7 @@ config.set(par, 'top', top)
 
 sil='Silenus'
 config.add_section(sil)
-config.set(sil, 'matrix', (0.334,0.334,0.334, 0))
+config.set(sil, 'matrix', matrix)
 
 plot='Plotting'
 config.add_section(plot)
