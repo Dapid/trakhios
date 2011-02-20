9a890fc668e650415c389fe9934c063aec359bfa
davidmenhur <davidmenhur@localhost>
2011-02-20 14:59:44 +0100

    Preparing GUI for choosing centers and cutoff values.

--- Image processing/load_image.py
+++ Image processing/load_image.py
@@ -1,7 +1,8 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from pylab import show, savefig
+import matplotlib.image as mpimg
+import numpy as np
 
 import silenus
 
@@ -9,20 +10,51 @@ def importer(name, it):
     image=mpimg.imread('data/'+silenus.namefile(name, it))
     return image
 
+def onclick(event):
+        print np.array([event.xdata, event.ydata])
 
-# Parameters
-namecode='Try2'
-n=197
+class positions:
+    def __init__(self):
+        self.centers=[]
+    def onclick(self, event):
+        self.centers.append(np.array([event.xdata, event.ydata]))
 
-frame=importer(namecode, n)
-plt.imshow(frame)
-plt.colorbar()
+print 'Starting'
+img0=importer('00', 55)
+m=len(img0)
+n=len(img0[0])
+img=np.ones((m*n,), dtype=np.int)
+img.resize(m,n)
+print 'Mixing channels'
+for i in xrange(m):
+    for j in xrange(n):
+        img[i][j]=silenus.mix_channels(img0[i][j])
 
-#print frame[788][137]
-frame2=frame[137:140][788:790]
-print frame2
-plt.imshow(frame2)
-plt.colorbar()
+print 'Plotting'
+fig = plt.figure()
+ax = fig.add_subplot(212)
+plt.hist(img.flatten(), 256, range=(0.0,1.0), fc='k', ec='k')
 
+ax = fig.add_subplot(211)
+cax=ax.imshow(img)
+fig.colorbar(cax)
 
-#show()
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+plt.show()
+fig.canvas.mpl_disconnect(cid)
+    
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.imshow(img)
+#
+#pos=positions
+#cid = fig.canvas.mpl_connect('button_press_event', pos.onclick)
+#
+#plt.show()
+#fig.canvas.mpl_disconnect(cid)
+
+print
+print 
+print 
+raw_input('End ')
+
