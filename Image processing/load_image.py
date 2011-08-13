12c88a51230cc29a6164d14bdd6b5f4fcb2e8879
davidmenhur <davidmenhur@localhost>
2011-08-13 23:39:20 +0200

    Improved interface layout

--- Image processing/load_image.py
+++ Image processing/load_image.py
@@ -21,7 +21,7 @@ class positions:
         x, y=(event.xdata, event.ydata)
         if 0<=x and 0<=y and y<=self.m and x<=self.n:
             self.centers.append([x, y])
-            print 'Got that!' 
+            print 'Got that!'
         else:
             print 'Invalid center. Try to click inside!'
 
@@ -29,7 +29,6 @@ print 'Starting'
 img0=importer('00', 55)
 m=len(img0)
 n=len(img0[0])
-
 print 'Mixing channels'
 
 img=silenus.mix_channels(img0)
@@ -37,25 +36,27 @@ img=silenus.mix_channels(img0)
 print 'Plotting'
 fig=plt.figure()
 
-ax=fig.add_subplot(211)
+h=10
+hist=3
+ax=plt.subplot2grid((h,h),(0,0), rowspan=h-hist, colspan=h)
 plt.title(r'$\mathrm{Image\ preview}$',size=30)
 imgplot=ax.imshow(img)
-imgplot.set_cmap('gist_gray')
+imgplot.set_cmap('gist_gray') # BW colors
 fig.colorbar(imgplot)
 
-ax=fig.add_subplot(212)
+ax=plt.subplot2grid((h,h),(h-hist+1,0), colspan=h, rowspan=hist)
 plt.title(r'$\mathrm{Histogram}$',size=15)
 plt.hist(img.flatten(), 256, fc='k', ec='k',
          histtype='stepfilled')
+plt.xlabel(r'$\mathrm{Click\ on\ the\ image\ to\ select\ '+
+           'tracking\ points.}$', size=16)
 
 pos=positions(m,n)
 cid = fig.canvas.mpl_connect('button_press_event', pos.onclick)
+fig.canvas.set_window_title('Trakhios::Select tracking points') 
 plt.show()
 fig.canvas.mpl_disconnect(cid)
 
-
-print
-print 
 print 
 raw_input('End ')
 
