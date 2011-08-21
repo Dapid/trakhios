8b502b6625edd4652785d2a3474a40110eb6b1ed
davidmenhur <davidmenhur@localhost>
2011-08-21 22:55:13 +0200

    MPlayer improvements

--- Image processing/MPlayer/lib_test.py
+++ Image processing/MPlayer/lib_test.py
@@ -7,7 +7,7 @@ class playerStream:
         self.p=Player(['-vo', 'png:z=3', 'P.mp4',
                        '-nosound'])#,'-fontconfig=0'])
         self.ispaused=False
-    
+        
     def pause(self):
         if not self.ispaused:
             self.p.pause()
@@ -18,38 +18,37 @@ class playerStream:
             self.p.pause()
             self.ispaused=False
             
+    def isend(self):
+        a=self.p.stream_end
+        b=self.p.stream_pos
+        
+        if a==None and b==None:
+            return True
+        else: return False
+
     def quit(self):
         self.p.quit()
 
 print 'Starting player'
-vid=playerStream()
 curr=os.curdir
-base=len(os.listdir(curr)) #
+base=len(os.listdir(curr))
+vid=playerStream()
 
 while True:
     dir=os.listdir(curr)
-    if len(dir)>base+30:
+    frames=len(dir)
+    if frames>base+30:
         vid.pause()
+        time.sleep(0.2)
     else:
         vid.resume()
+        time.sleep(0.5)
     
-    time.sleep(0.2)
-    if len(dir)==base:          # If no frames left
-        dir=os.listdir(curr)    # give it some time
-        if len(dir)==base:      # and break.
-            break               # Maybe lib could provide it.
+    if frames==base:        # If no frames left
+        if vid.isend():     # and it is the end,
+            break           # exit
 
 print 'End reached.'
 vid.quit()
 print 'Quitted.'
  
-"""
-print 'A'
-p = Player(['-vo', 'png', 'P.mp4'])
-print 'B'
-raw_input()
-print 'C'
-p.pause()
-print 'D'
-raw_input('End')
-"""
